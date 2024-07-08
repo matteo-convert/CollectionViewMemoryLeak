aa5992d8d8b51c464ad824007c860acd8e35c1bc
Matteo Convert <matteo.convert@enodea.com>
2024-07-08 09:40:23 +0200

    update > .Net 8

--- CollectionViewMemoryLeak/CollectionViewMemoryLeak/ViewModels/CollectionViewViewModel.cs
+++ CollectionViewMemoryLeak/CollectionViewMemoryLeak/ViewModels/CollectionViewViewModel.cs
@@ -1,8 +1,12 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CollectionViewMemoryLeak.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using NodaLibrary.NodaHelpers.Diagnostic;
 using NodaMobileSales.Maui.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +16,25 @@ namespace CollectionViewMemoryLeak.ViewModels
     public partial class CollectionViewViewModel : ViewModelBase
     {
 
+        [ObservableProperty]
+        private ObservableCollection<CDE_ENT> _list;
         public CollectionViewViewModel(NavigationService navigationService) : base(navigationService)
         {
             Title = "CollectionView Page";
             Counter.Increment(this.GetType().Name);
+            LoadListCde();
         }
 
         ~CollectionViewViewModel()
         {
             Counter.Decrement(this.GetType().Name);
+        }
+        private void LoadListCde()
+        {
+            var list = JsonConvert.DeserializeObject<List<CDE_ENT>>(File.ReadAllText(@"C:\Users\matco\Downloads\cde_ent.json"));
+
+            List = new ObservableCollection<CDE_ENT>(list);
+
         }
 
 
--- CollectionViewMemoryLeak/CollectionViewMemoryLeak/ViewModels/ListViewViewModel.cs
+++ CollectionViewMemoryLeak/CollectionViewMemoryLeak/ViewModels/ListViewViewModel.cs
@@ -1,9 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CollectionViewMemoryLeak.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using NodaLibrary.NodaHelpers.Diagnostic;
 using NodaMobileSales.Maui.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +16,22 @@ namespace CollectionViewMemoryLeak.ViewModels
     public partial class ListViewViewModel : ViewModelBase
     {
 
+        [ObservableProperty]
+        private ObservableCollection<CDE_ENT> _list;
 
         public ListViewViewModel(NavigationService navigationService) : base(navigationService)
         {
             Title = "ListView Page";
             Counter.Increment(this.GetType().Name);
+            LoadListCde();
+        }
+
+        private void LoadListCde()
+        {
+            var list = JsonConvert.DeserializeObject<List<CDE_ENT>>(File.ReadAllText(@"C:\Users\matco\Downloads\cde_ent.json"));
+
+            List = new ObservableCollection<CDE_ENT>(list);
+
         }
 
         ~ListViewViewModel()
